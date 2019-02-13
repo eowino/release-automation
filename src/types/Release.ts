dcfc960c7de821c914a404afcbc2871d2f051946
Evans Owino <evansowino@hotmail.com>
2019-02-13 21:59:32 +0000

    feat: types update

--- src/types/Release.ts
+++ src/types/Release.ts
@@ -1,8 +1,12 @@
 export interface IRealease {
   /** The name of the branch you want to create a release from */
   branchName: string;
+  /** The base branch you want to merge branches into. Default is master. */
+  baseBranch: string;
   /** The list of branches to branch into branchName */
   branchesToMerge: string[];
   /** Last logged error while executing */
   error?: string;
+  /** The list of branches already merged */
+  mergedBranches?: string[];
 }
