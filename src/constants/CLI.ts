d078ff73cebbabfcac77b6a842d638c831d10cb7
Evans Owino <evansowino@hotmail.com>
2019-02-17 17:44:09 +0000

    fix: spacing between emojis

--- src/constants/CLI.ts
+++ src/constants/CLI.ts
@@ -1,27 +1,27 @@
 export const MUST_BE_GIT_REPO =
-  '🚫 Sorry! This CLI should be run on a git instatiated directory.';
+  '🚫  Sorry! This CLI should be run on a git instatiated directory.';
 
-export const GOODBYE = 'Goodbye ✌️';
+export const GOODBYE = 'Goodbye  ✌ ️';
 
-export const BEGIN_MERGE = '🔗 Beginning merge process';
+export const BEGIN_MERGE = '🔗  Beginning merge process';
 
 export const EXIT_AFTER_MERGE_FAIL =
-  '🚫 It seems a merge failed. The release process will terminate to allow you to resolve the issue.';
+  '🚫  It seems a merge failed. The release process will terminate to allow you to resolve the issue.';
 
 export const MUST_SELECT_NEXT_VERSION =
-  '🚫 You must select the next release version.';
+  '🚫  You must select the next release version.';
 
 export const UNABLE_TO_SET_NPM_VERSION =
-  '🚫 Unable to set the next NPM Version.';
+  '🚫  Unable to set the next NPM Version.';
 
-export const SETTING_NEXT_NPM_VERSION = '🔧 Setting the next NPM version';
+export const SETTING_NEXT_NPM_VERSION = '🔧  Setting the next NPM version';
 
-export const PUSHING_GIT_TAGS = '🏷 Pusing git tags';
+export const PUSHING_GIT_TAGS = '🏷  Pusing git tags';
 
-export const CHECKOUT_PREPROD_BRANCH = '👟 Switching to the preprod branch';
+export const CHECKOUT_PREPROD_BRANCH = '👟  Switching to the preprod branch';
 
-export const MERGE_BRANCH_INTO_PREPROD = '🔗 Merging branch into preprod';
+export const MERGE_BRANCH_INTO_PREPROD = '🔗  Merging branch into preprod';
 
-export const PUSHING_PREPROD_BRANCH = '➫ Pushing preprod branch to remote';
+export const PUSHING_PREPROD_BRANCH = '➫  Pushing preprod branch to remote';
 
-export const RELEASE_PROCESS_FINISHED = '🏁 Release process finished 🎉';
+export const RELEASE_PROCESS_FINISHED = '🏁  Release process finished  🎉';
