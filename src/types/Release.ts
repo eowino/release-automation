ca98fc075aa51eadbaef1a4414ba030585d9e594
Evans Owino <Owin79893@GB-LOC-AL-25403.local>
2019-02-20 12:23:50 +0000

    chore: interface updates

--- src/types/Release.ts
+++ src/types/Release.ts
@@ -1,12 +1,20 @@
-export interface IRealease {
-  /** The name of the branch you want to create a release from */
-  branchName: string;
-  /** The base branch you want to merge branches into. Default is master. */
+export interface IRelease {
+  /** The branch you always want to create a new branch from. Default is master. */
   baseBranch: string;
+}
+
+export interface IRealeaseProgress extends IRelease {
+  /** The branch you always want to create a new branch from. */
+  branchName: string;
   /** The list of branches to branch into branchName */
   branchesToMerge: string[];
   /** Last logged error while executing */
   error?: string;
   /** The list of branches already merged */
   mergedBranches?: string[];
+}
+
+export interface IRealeaseConfig extends IRelease {
+  /** Whether the edit release notes Github page should be opened when the process completes */
+  open?: boolean;
 }
